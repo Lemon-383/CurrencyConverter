8d9beea8b99f1380334e42cf9e404970f3c84fe6
Lemon-383 <47275432+Lemon-383@users.noreply.github.com>
2020-06-04 18:03:35 +0300

    Update CurrencyConverter.py

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,6 +1,6 @@
 # Python 3.8
 
-def inputInt(prompt = ""):
+def inputInt(prompt=""):
     while True:
         try:
             res = int(input(prompt))
@@ -9,7 +9,7 @@ def inputInt(prompt = ""):
             print("Invalid input")
 
 
-def inputFloat(prompt = ""):
+def inputFloat(prompt=""):
     while True:
         try:
             res = float(input(prompt))
@@ -25,62 +25,57 @@ def Menu(menuItems):
         i += 1
 
     while True:
-    	out = inputInt("Your choise: ")
-    	if out >= 0 and out < len(menuItems):
-    		return out
-    	else:
-    		print("Invalid input")
-
+        out = inputInt("Your choise: ")
+        if out >= 0 and out < len(menuItems):
+            return out
+        else:
+            print("Invalid input")
 
 
 def Main():
     currency = ["USD", "EUR", "UAH", "PLN", "BYR", "CNY", "RUB"]
-    
+
     print("Convert from:")
     convFrom = Menu(currency)
     print("\n" * 2)
 
     print("Convert to:")
-    convTo = Menu(currency)
-
-    if convTo == convFrom:
-    	while not (convTo >= 0 and convTo < len(currency) and convFrom != convTo):
-    		print("Invalid input")
-    		convTo = inputInt("Your choise: ")
+    convTo = Menu(list(currency[0: convFrom]) +
+                  list(currency[convFrom + 1: len(currency)]))
     print("\n" * 2)
-    				
 
     convValue = 0
     while True:
-    	convValue = inputFloat(f"Enter amoun in {currency[convFrom]}: ")
-    	if convValue > 0:
-    		break
-    	else:
-    		print("Invalid input")
+        convValue = inputFloat(f"Enter amoun in {currency[convFrom]}: ")
+        if convValue > 0:
+            break
+        else:
+            print("Invalid input")
 
 
     print("\nChoose an input option: ")
-    choise = Menu([f"Enter {currency[convFrom]} cost in {currency[convTo]}: ", f"Enter {currency[convTo]} cost in {currency[convFrom]}: "])
+    choise = Menu([f"Enter {currency[convFrom]} cost in {currency[convTo]}: ",
+                   f"Enter {currency[convTo]} cost in {currency[convFrom]}: "])
     print()
 
     convRate = 0
     if choise == 0:
-    	while True:
-    		convRate = inputFloat(f"Enter {currency[convFrom]} cost in {currency[convTo]}: ")
-    		if convRate > 0:
-    			break
-    		else:
-    			print("Invalid input")
-    	convResult = convValue * convRate
+        while True:
+            convRate = inputFloat(f"Enter {currency[convFrom]} cost in {currency[convTo]}: ")
+            if convRate > 0:
+                break
+            else:
+                print("Invalid input")
+        convResult = convValue * convRate
 
     elif choise == 1:
-    	while True:
-    		convRate = inputFloat(f"Enter {currency[convTo]} cost in {currency[convFrom]}: ")
-    		if convRate > 0:
-    			break
-    		else:
-    			print("Invalid input")
-    	convResult = convValue / convRate
+        while True:
+            convRate = inputFloat(f"Enter {currency[convTo]} cost in {currency[convFrom]}: ")
+            if convRate > 0:
+                break
+            else:
+                print("Invalid input")
+        convResult = convValue / convRate
 
     print(str(round(convValue, 2)), currency[convFrom], "=", str(round(convResult, 2)), currency[convTo])
 
@@ -88,8 +83,8 @@ def Main():
 Main()
 convAgain = 'n'
 while True:
-	convAgain = input("\nConvert again? (y/n): ")
-	if convAgain == 'y':
-		Main()
-	else:
-		break
+    convAgain = input("\nConvert again? (y/n): ")
+    if convAgain == 'y':
+        Main()
+    else:
+        break
